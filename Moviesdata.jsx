cc0507b3158c945b2aac70d523f899a75f23a4ca
Rakshithb123 <120550181+Rakshithb123@users.noreply.github.com>
2023-03-10 10:25:04 +0530

    Add files via upload

---
+++ Moviesdata.jsx
@@ -0,0 +1,230 @@
+import React from "react";
+import './global.css'
+
+const Moviesdata=(props)=>{
+    return (
+              <div id="parent">
+                <h1 className="moviehead">WELCOME TO MOVIE PAGE</h1>
+                <div className="main-block">
+                    <div className="sub-block">
+                    <img src={props.data[0].image1} alt="film" />
+                    <h3>{props.data[0].name}</h3>
+                    <h4>{props.data[0].actor}</h4>
+                    <h4>{props.data[0].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[1].image1} alt="film" />
+                    <h3>{props.data[1].name}</h3>
+                    <h4>{props.data[1].actor}</h4>
+                    <h4>{props.data[1].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[2].image1} alt="film" />
+                    <h3>{props.data[2].name}</h3>
+                    <h4>{props.data[2].actor}</h4>
+                    <h4>{props.data[2].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[3].image1} alt="film" />
+                    <h3>{props.data[3].name}</h3>
+                    <h4>{props.data[3].actor}</h4>
+                    <h4>{props.data[3].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[4].image1} alt="film" />
+                    <h3>{props.data[4].name}</h3>
+                    <h4>{props.data[4].actor}</h4>
+                    <h4>{props.data[4].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[5].image1} alt="film" />
+                    <h3>{props.data[5].name}</h3>
+                    <h4>{props.data[5].actor}</h4>
+                    <h4>{props.data[5].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[6].image1} alt="film" />
+                    <h3>{props.data[6].name}</h3>
+                    <h4>{props.data[6].actor}</h4>
+                    <h4>{props.data[6].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[7].image1} alt="film" />
+                    <h3>{props.data[7].name}</h3>
+                    <h4>{props.data[7].actor}</h4>
+                    <h4>{props.data[7].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[8].image1} alt="film" />
+                    <h3>{props.data[8].name}</h3>
+                    <h4>{props.data[8].actor}</h4>
+                    <h4>{props.data[8].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[9].image1} alt="film" />
+                    <h3>{props.data[9].name}</h3>
+                    <h4>{props.data[9].actor}</h4>
+                    <h4>{props.data[9].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[10].image1} alt="film" />
+                    <h3>{props.data[10].name}</h3>
+                    <h4>{props.data[10].actor}</h4>
+                    <h4>{props.data[10].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[11].image1} alt="film" />
+                    <h3>{props.data[11].name}</h3>
+                    <h4>{props.data[11].actor}</h4>
+                    <h4>{props.data[11].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[12].image1} alt="film" />
+                    <h3>{props.data[12].name}</h3>
+                    <h4>{props.data[12].actor}</h4>
+                    <h4>{props.data[12].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[13].image1} alt="film" />
+                    <h3>{props.data[13].name}</h3>
+                    <h4>{props.data[13].actor}</h4>
+                    <h4>{props.data[13].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[14].image1} alt="film" />
+                    <h3>{props.data[14].name}</h3>
+                    <h4>{props.data[14].actor}</h4>
+                    <h4>{props.data[14].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[15].image1} alt="film" />
+                    <h3>{props.data[15].name}</h3>
+                    <h4>{props.data[15].actor}</h4>
+                    <h4>{props.data[15].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[16].image1} alt="film" />
+                    <h3>{props.data[16].name}</h3>
+                    <h4>{props.data[16].actor}</h4>
+                    <h4>{props.data[16].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[17].image1} alt="film" />
+                    <h3>{props.data[17].name}</h3>
+                    <h4>{props.data[17].actor}</h4>
+                    <h4>{props.data[17].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[18].image1} alt="film" />
+                    <h3>{props.data[18].name}</h3>
+                    <h4>{props.data[18].actor}</h4>
+                    <h4>{props.data[18].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[19].image1} alt="film" />
+                    <h3>{props.data[19].name}</h3>
+                    <h4>{props.data[19].actor}</h4>
+                    <h4>{props.data[19].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[20].image1} alt="film" />
+                    <h3>{props.data[20].name}</h3>
+                    <h4>{props.data[20].actor}</h4>
+                    <h4>{props.data[20].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[21].image1} alt="film" />
+                    <h3>{props.data[21].name}</h3>
+                    <h4>{props.data[21].actor}</h4>
+                    <h4>{props.data[21].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[22].image1} alt="film" />
+                    <h3>{props.data[22].name}</h3>
+                    <h4>{props.data[22].actor}</h4>
+                    <h4>{props.data[22].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[23].image1} alt="film" />
+                    <h3>{props.data[23].name}</h3>
+                    <h4>{props.data[23].actor}</h4>
+                    <h4>{props.data[23].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[24].image1} alt="film" />
+                    <h3>{props.data[24].name}</h3>
+                    <h4>{props.data[24].actor}</h4>
+                    <h4>{props.data[24].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[25].image1} alt="film" />
+                    <h3>{props.data[25].name}</h3>
+                    <h4>{props.data[25].actor}</h4>
+                    <h4>{props.data[25].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[26].image1} alt="film" />
+                    <h3>{props.data[26].name}</h3>
+                    <h4>{props.data[26].actor}</h4>
+                    <h4>{props.data[26].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[27].image1} alt="film" />
+                    <h3>{props.data[27].name}</h3>
+                    <h4>{props.data[27].actor}</h4>
+                    <h4>{props.data[27].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[28].image1} alt="film" />
+                    <h3>{props.data[28].name}</h3>
+                    <h4>{props.data[28].actor}</h4>
+                    <h4>{props.data[28].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[29].image1} alt="film" />
+                    <h3>{props.data[29].name}</h3>
+                    <h4>{props.data[29].actor}</h4>
+                    <h4>{props.data[29].yor}</h4>
+                    </div>
+
+                    <div className="sub-block">
+                    <img src={props.data[30].image1} alt="film" />
+                    <h3>{props.data[30].name}</h3>
+                    <h4>{props.data[30].actor}</h4>
+                    <h4>{props.data[30].yor}</h4>
+                    </div>
+                </div>
+              </div>
+    )
+}
+
+export default Moviesdata
